68ac327657a896e1ffa0eb3e07443e0a8351cbd6
Carolina RZ7 <racolza@hotmail.com>
2022-06-21 12:28:59 -0500

    ultimo commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -30,11 +30,11 @@ function conection(status) {
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
   if (status===1) {
-    return 'online';
+    return 'Online';
   } else if (status===2) {
-  return 'away'; 
+  return 'Away'; 
   } else {
-   return 'offline';
+   return 'Offline';
   }
 }
 
@@ -159,30 +159,45 @@ function esVerdadero(valor){
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
   if (valor===true) {
-    return 'soy verdadero';
+    return 'Soy verdadero';
   } 
-  return 'soy falso';
+  return 'Soy falso';
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
-
-  
+  let arrayTablaDel6 = []
+  for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
+  }
+  return arrayTablaDel6
+ 
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
-  
+  if(numero > 99 && numero < 1000){
+    return true
+  }
+  return false
+   
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var a = numero;
+  var i = 0;
+  do {
+    i = i + 1;
+    a = a + 5;
+  }
+  while(i < 8);
+  return a;
 }
 
 
